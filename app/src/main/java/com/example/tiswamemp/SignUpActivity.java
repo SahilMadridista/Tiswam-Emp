c3ca42f7e7e4a3f9f4d3e7145837cfa0c439d548
sahil <sahil91098singh@gmail.com>
2020-10-16 13:20:47 +0530

    google-services.json added.

--- app/src/main/java/com/example/tiswamemp/SignUpActivity.java
+++ app/src/main/java/com/example/tiswamemp/SignUpActivity.java
@@ -3,13 +3,23 @@ package com.example.tiswamemp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
+
+   Spinner spinner;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_sign_up);
+
+      spinner = findViewById(R.id.post_spinner);
+      ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+              R.array.posts, R.layout.spinner_item);
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+      spinner.setAdapter(adapter);
 
 
 
